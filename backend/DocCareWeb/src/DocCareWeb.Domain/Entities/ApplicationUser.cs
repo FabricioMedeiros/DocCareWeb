54e956638deae3931a4e4697827be70f65e16d9f
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-07-10 11:03:29 -0300

    Instalacao do pacote do Identity na camada Domain, e criacao da model ApplicationUser

---
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DocCareWeb.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string Name { get; set; }
+    }
+}
