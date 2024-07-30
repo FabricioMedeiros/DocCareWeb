21708a7e89f5d4005df2b1832897515f46540d94
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-07-30 20:23:14 -0300

    Criacao do servico basico responsavel por implementar os metodos do padrao Notifier

---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Services/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using DocCareWeb.Application.Notifications;
+
+namespace DocCareWeb.Application.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificator _notificator;
+
+        protected BaseService(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected void Notify(string message)
+        {
+            _notificator.AddNotification(new Notification(message));
+        }
+
+        protected bool IsValidOperation()
+        {
+            return !_notificator.HasNotification();
+        }
+    }
+}
