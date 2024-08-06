dd672d19a5d789704a2cd4344e18c2be6e5aeefd
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-06 14:27:20 -0300

    Criacao da interface e do servico de geracao do JWT

---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,10 @@
+﻿using DocCareWeb.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace DocCareWeb.Application.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        Task<string> GenerateToken(ApplicationUser user);
+    }
+}
---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Services/JwtTokenService.cs
@@ -0,0 +1,63 @@
+﻿using DocCareWeb.Application.Configurations;
+using DocCareWeb.Application.Interfaces;
+using DocCareWeb.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DocCareWeb.Application.Services
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public JwtTokenService(IOptions<JwtSettings> jwtSettings, UserManager<ApplicationUser> userManager)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateToken(ApplicationUser user)
+        {
+            var claims = await GetClaims(user);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpirationInMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<IList<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("username", user.Name),
+            };
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            claims.AddRange(userClaims);
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+    }
+}
