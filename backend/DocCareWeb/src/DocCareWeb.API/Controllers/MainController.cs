eff2eda04d3970a1ae23e681f72333d83eae0636
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-06 14:32:16 -0300

    Criacao da MainController com a implementacao dos metodos referentes as notificacoes

---
+++ backend/DocCareWeb/src/DocCareWeb.API/Controllers/MainController.cs
@@ -0,0 +1,53 @@
+﻿using DocCareWeb.Application.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DocCareWeb.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        private readonly INotificator _notificator;
+
+        protected MainController(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected ActionResult CustomResponse(object? result = null)
+        {
+            if (_notificator.HasNotification())
+            {
+                return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+                {
+                    { "Messages", _notificator.GetNotifications().Select(n => n.Message).ToArray() }
+                }));
+            }
+
+            return Ok(result);
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid)
+                NotifyModelStateErrors(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotifyModelStateErrors(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                var errorMsg = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+                NotifyError(errorMsg);
+            }
+        }
+
+        protected void NotifyError(string message)
+        {
+            _notificator.AddNotification(new Notification(message));
+        }
+    }
+}
