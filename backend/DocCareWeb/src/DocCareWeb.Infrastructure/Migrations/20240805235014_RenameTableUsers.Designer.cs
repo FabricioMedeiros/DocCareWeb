85766e618fc2ca3e069fd64715bb8f3260b10013
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-08 14:50:32 -0300

    Renomeacao da tabela users para Users

--- backend/DocCareWeb/src/DocCareWeb.Infrastructure/Migrations/20240805235014_RenameTableUsers.Designer.cs
+++ backend/DocCareWeb/src/DocCareWeb.Infrastructure/Migrations/20240805235014_RenameTableUsers.Designer.cs
@@ -4,6 +4,7 @@ using DocCareWeb.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DocCareWeb.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240805235014_RenameTableUsers")]
+    partial class RenameTableUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -246,6 +249,10 @@ namespace DocCareWeb.Infrastructure.Migrations
                         .IsRequired()
                         .HasMaxLength(60)
                         .HasColumnType("nvarchar(60)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
