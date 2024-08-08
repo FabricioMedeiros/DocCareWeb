28fd281d343da4513f19b9e601840673ac3444a6
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-08 14:51:44 -0300

    Remocao da propriedade Name da entidade HealthPlan

--- backend/DocCareWeb/src/DocCareWeb.Domain/Entities/HealthPlan.cs
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/HealthPlan.cs
@@ -2,7 +2,6 @@
 {
     public class HealthPlan : BaseEntity
     {
-        public required string Name { get; set; }
         public string? Description { get; set; }
         public required decimal Cost { get; set; }
         public ICollection<Patient>? Patients { get; set; }
