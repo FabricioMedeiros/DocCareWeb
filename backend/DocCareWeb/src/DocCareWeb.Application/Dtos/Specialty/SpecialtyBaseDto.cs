31e0f075c7424c6d08b2403b6c155416c98df59e
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-09 09:54:32 -0300

    Remocao da propriedade Name da SpecialtyBaseDto

--- backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Specialty/SpecialtyBaseDto.cs
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Specialty/SpecialtyBaseDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace DocCareWeb.Application.Dtos.Specialty
 {
     public abstract class SpecialtyBaseDto
-    {
-        public required string Name { get; set; }
-        public string? Description { get; set; }
+    { 
+        public required string Description { get; set; }
     }
 }
