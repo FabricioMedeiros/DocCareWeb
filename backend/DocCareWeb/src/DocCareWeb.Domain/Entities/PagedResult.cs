4795c5201679dc1b9362d4b13c7c6e6cb44d372c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-09 10:42:47 -0300

    Alterada a ordem das propriedades do retorno de paginacao

--- backend/DocCareWeb/src/DocCareWeb.Domain/Entities/PagedResult.cs
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/PagedResult.cs
@@ -2,8 +2,8 @@
 {
     public class PagedResult<TDto>
     {
-        public int PageSize { get; set; }
         public int Page { get; set; }
+        public int PageSize { get; set; }
         public int TotalRecords { get; set; }
         public IEnumerable<TDto>? Items { get; set; }
     }
