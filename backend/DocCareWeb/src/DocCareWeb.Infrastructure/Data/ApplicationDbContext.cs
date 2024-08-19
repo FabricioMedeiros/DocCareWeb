77377384945701fddbd26bd44b8c778a8a44b174
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-19 09:45:02 -0300

    Refatoracao no tamanho de campos das tabelas Patient e Doctor, e organizacao das configuracoes por ondem alfabetica das tabelas.

--- backend/DocCareWeb/src/DocCareWeb.Infrastructure/Data/ApplicationDbContext.cs
+++ backend/DocCareWeb/src/DocCareWeb.Infrastructure/Data/ApplicationDbContext.cs
@@ -27,81 +27,16 @@ namespace DocCareWeb.Infrastructure.Data
                 entity.Property(e => e.Name).IsRequired().HasMaxLength(60); 
             });
 
-            modelBuilder.Entity<Doctor>(entity =>
-            {
-                entity.HasKey(d => d.Id);
-                entity.Property(d => d.Name).IsRequired().HasMaxLength(80);
-                entity.Property(d => d.Crm).IsRequired().HasMaxLength(20);
-                entity.Property(d => d.CellPhone).IsRequired().HasMaxLength(11);
-                entity.Property(d => d.Email).IsRequired().HasMaxLength(100);
-                entity.HasMany(d => d.Appointments)
-                      .WithOne(a => a.Doctor)
-                      .HasForeignKey(a => a.DoctorId)
-                      .OnDelete(DeleteBehavior.Restrict);
-
-                entity.HasOne(d => d.Specialty)
-                      .WithMany(s => s.Doctors)
-                      .HasForeignKey(d => d.SpecialtyId)
-                      .OnDelete(DeleteBehavior.Restrict);
-            });
-
-            modelBuilder.Entity<Patient>(entity =>
-            {
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.Name).IsRequired().HasMaxLength(80);
-                entity.Property(p => p.Cpf).HasMaxLength(11);
-                entity.Property(p => p.Rg).HasMaxLength(20);
-                entity.Property(p => p.Gender).IsRequired();
-                entity.Property(p => p.BirthDate).IsRequired();
-                entity.Property(p => p.Phone).HasMaxLength(11);
-                entity.Property(p => p.CellPhone).HasMaxLength(11);
-                entity.Property(p => p.Email).HasMaxLength(100);
-                entity.Property(p => p.CreatedAt).IsRequired();
-                entity.Property(p => p.CreatedBy).IsRequired();
-                entity.Property(p => p.Notes).HasMaxLength(5000);
-                entity.HasOne(p => p.Address)
-                      .WithMany()
-                      .HasForeignKey(p => p.AddressId)
-                      .OnDelete(DeleteBehavior.Cascade);
-                entity.HasOne(p => p.HealthPlan)
-                      .WithMany(hp => hp.Patients)
-                      .HasForeignKey(p => p.HealthPlanId)
-                      .OnDelete(DeleteBehavior.Restrict);
-                entity.HasMany(p => p.Appointments)
-                      .WithOne(a => a.Patient)
-                      .HasForeignKey(a => a.PatientId)
-                      .OnDelete(DeleteBehavior.Restrict);
-            });
-
-            modelBuilder.Entity<Specialty>(entity =>
-            {
-                entity.HasKey(s => s.Id);
-                entity.Property(s => s.Description).IsRequired().HasMaxLength(50);
-                entity.HasMany(s => s.Doctors)
-                      .WithOne(d => d.Specialty)
-                      .HasForeignKey(d => d.SpecialtyId)
-                      .OnDelete(DeleteBehavior.Restrict);
-            });
-
             modelBuilder.Entity<Address>(entity =>
             {
                 entity.HasKey(a => a.Id);
-                entity.Property(a => a.Street).IsRequired().HasMaxLength(200);
-                entity.Property(a => a.Neighborhood).IsRequired().HasMaxLength(100);
-                entity.Property(a => a.City).IsRequired().HasMaxLength(100);
-                entity.Property(a => a.State).IsRequired().HasMaxLength(50);
-                entity.Property(a => a.ZipCode).IsRequired().HasMaxLength(20);
-            });
-
-            modelBuilder.Entity<HealthPlan>(entity =>
-            {
-                entity.HasKey(hp => hp.Id);
-                entity.Property(hp => hp.Description).IsRequired().HasMaxLength(60);
-                entity.Property(hp => hp.Cost).HasColumnType("decimal(18,2)");
-                entity.HasMany(hp => hp.Patients)
-                      .WithOne(p => p.HealthPlan)
-                      .HasForeignKey(p => p.HealthPlanId)
-                      .OnDelete(DeleteBehavior.Restrict);
+                entity.Property(a => a.Street).IsRequired().HasMaxLength(50);
+                entity.Property(a => a.Number).HasMaxLength(10);
+                entity.Property(a => a.Complement).HasMaxLength(20);
+                entity.Property(a => a.Neighborhood).IsRequired().HasMaxLength(50);
+                entity.Property(a => a.City).IsRequired().HasMaxLength(50);
+                entity.Property(a => a.State).IsRequired().HasMaxLength(2);
+                entity.Property(a => a.ZipCode).IsRequired().HasMaxLength(8);
             });
 
             modelBuilder.Entity<Appointment>(entity =>
@@ -132,6 +67,73 @@ namespace DocCareWeb.Infrastructure.Data
                       .HasForeignKey(a => a.HealthPlanId)
                       .OnDelete(DeleteBehavior.Restrict);
             });
+
+            modelBuilder.Entity<Doctor>(entity =>
+            {
+                entity.HasKey(d => d.Id);
+                entity.Property(d => d.Name).IsRequired().HasMaxLength(50);
+                entity.Property(d => d.Crm).IsRequired().HasMaxLength(20);
+                entity.Property(d => d.CellPhone).IsRequired().HasMaxLength(11);
+                entity.Property(d => d.Email).IsRequired().HasMaxLength(60);
+                entity.HasMany(d => d.Appointments)
+                      .WithOne(a => a.Doctor)
+                      .HasForeignKey(a => a.DoctorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(d => d.Specialty)
+                      .WithMany(s => s.Doctors)
+                      .HasForeignKey(d => d.SpecialtyId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Name).IsRequired().HasMaxLength(50);
+                entity.Property(p => p.Cpf).HasMaxLength(11);
+                entity.Property(p => p.Rg).HasMaxLength(20);
+                entity.Property(p => p.Gender).IsRequired();
+                entity.Property(p => p.BirthDate).IsRequired();
+                entity.Property(p => p.Phone).HasMaxLength(10);
+                entity.Property(p => p.CellPhone).HasMaxLength(11);
+                entity.Property(p => p.Email).HasMaxLength(60);
+                entity.Property(p => p.CreatedAt).IsRequired();
+                entity.Property(p => p.CreatedBy).IsRequired();
+                entity.Property(p => p.Notes).HasMaxLength(5000);
+                entity.HasOne(p => p.Address)
+                      .WithMany()
+                      .HasForeignKey(p => p.AddressId)
+                      .OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(p => p.HealthPlan)
+                      .WithMany(hp => hp.Patients)
+                      .HasForeignKey(p => p.HealthPlanId)
+                      .OnDelete(DeleteBehavior.Restrict);
+                entity.HasMany(p => p.Appointments)
+                      .WithOne(a => a.Patient)
+                      .HasForeignKey(a => a.PatientId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<HealthPlan>(entity =>
+            {
+                entity.HasKey(hp => hp.Id);
+                entity.Property(hp => hp.Description).IsRequired().HasMaxLength(60);
+                entity.Property(hp => hp.Cost).HasColumnType("decimal(18,2)");
+                entity.HasMany(hp => hp.Patients)
+                      .WithOne(p => p.HealthPlan)
+                      .HasForeignKey(p => p.HealthPlanId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Specialty>(entity =>
+            {
+                entity.HasKey(s => s.Id);
+                entity.Property(s => s.Description).IsRequired().HasMaxLength(50);
+                entity.HasMany(s => s.Doctors)
+                      .WithOne(d => d.Specialty)
+                      .HasForeignKey(d => d.SpecialtyId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });            
         }
     }
 }
