9f563cd27d41aea7c32233a8ce0eacd973759318
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-19 10:27:52 -0300

    Criacao dos contrutores das dtos de enderecos, esses contrutores se fazem necessarios devido a dto de endereco ser uma entidade aninhada da entidade do paciente

--- backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Address/AddressCreateDto.cs
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Address/AddressCreateDto.cs
@@ -2,6 +2,8 @@
 {
     public class AddressCreateDto : AddressBaseDto
     {
-
+        public AddressCreateDto(string street, string number, string? complement, string neighborhood, string city, string state, string zipCode) : base(street, number, complement, neighborhood, city, state, zipCode)
+        {
+        }
     }
 }
--- backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Address/AddressUpdateDto.cs
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Address/AddressUpdateDto.cs
@@ -2,6 +2,10 @@
 {
     public class AddressUpdateDto : AddressBaseDto
     {
+        public AddressUpdateDto(string street, string number, string? complement, string neighborhood, string city, string state, string zipCode) : base(street, number, complement, neighborhood, city, state, zipCode)
+        {
+        }
+
         public int Id { get; set; }
 
     }
