4d4aa88b20e8baeda672db3c3ee3fca410850025
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-19 09:58:16 -0300

    Alteracao do nome da propriedade ExpirationInMinutes para ExpiryInMinutes na entidade responsavel por armazenar as informacoes do Jwt.

--- backend/DocCareWeb/src/DocCareWeb.Application/Configurations/JwtSettings.cs
+++ backend/DocCareWeb/src/DocCareWeb.Application/Configurations/JwtSettings.cs
@@ -5,6 +5,6 @@
         public required string SecretKey { get; set; }
         public required string Issuer { get; set; }
         public required string Audience { get; set; }
-        public required int ExpirationInMinutes { get; set; }
+        public required int ExpiryInMinutes { get; set; }
     }
 }
