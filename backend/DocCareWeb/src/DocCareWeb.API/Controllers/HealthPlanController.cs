4a68983b76d5fa1e5be9138a5664644e5376fab9
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-19 09:59:09 -0300

    Criacao da controller de planos de saude

---
+++ backend/DocCareWeb/src/DocCareWeb.API/Controllers/HealthPlanController.cs
@@ -0,0 +1,73 @@
+﻿using DocCareWeb.Application.Dtos.HealthPlan;
+using DocCareWeb.Application.Interfaces;
+using DocCareWeb.Application.Notifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocCareWeb.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthPlanController : MainController
+    {
+        private readonly IHealthPlanService _healthPlanService;
+
+        public HealthPlanController(IHealthPlanService healthPlanService, INotificator notificator)
+            : base(notificator)
+        {
+            _healthPlanService = healthPlanService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] Dictionary<string, string>? filters, [FromQuery] int? pageNumber = null, [FromQuery] int? pageSize = null)
+        {
+            var healthPlans = await _healthPlanService.GetAllAsync(filters, pageNumber, pageSize);
+            return CustomResponse(healthPlans);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var healthPlan = await _healthPlanService.GetByIdAsync(id);
+
+            if (healthPlan == null) return NotFound();
+
+            return CustomResponse(healthPlan);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] HealthPlanCreateDto healthPlanDto)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var createdHealhPlan = await _healthPlanService.AddAsync(healthPlanDto);
+            return CustomResponse(createdHealhPlan);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] HealthPlanUpdateDto healthPlanDto)
+        {
+            if (id != healthPlanDto.Id)
+            {
+                NotifyError("O ID informado não é o mesmo que foi passado na query.");
+                return CustomResponse();
+            }
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _healthPlanService.UpdateAsync(healthPlanDto);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var healthPlan = await _healthPlanService.GetByIdAsync(id);
+
+            if (healthPlan == null) return NotFound();
+
+            await _healthPlanService.DeleteAsync(id);
+
+            return CustomResponse();
+        }
+    }
+}
