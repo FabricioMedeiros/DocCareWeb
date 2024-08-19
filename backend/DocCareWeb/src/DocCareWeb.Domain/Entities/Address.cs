8bc91cb9e3180765c5409ad2cc4190bab5570d77
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-19 09:45:39 -0300

    Remocao da propriedade Country da entidade Address

--- backend/DocCareWeb/src/DocCareWeb.Domain/Entities/Address.cs
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/Address.cs
@@ -3,10 +3,11 @@
     public class Address : BaseEntity
     {
         public required string Street { get; set; }
+        public string? Number { get; set; }
+        public string? Complement { get; set; }
         public required string City { get; set; }
         public string? Neighborhood { get; set; }        
         public required string State { get; set; }
         public required string ZipCode { get; set; }
-        public string? Country { get; set; }
     }
 }
