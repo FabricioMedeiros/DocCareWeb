681500b747e9da84d5e2f160d0cf13dd21f6090a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-30 11:24:24 -0300

    Criacao da dto AppointmentStatusDto para atualizacao do status da consulta

---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Appointment/AppointmentStatusDto.cs
@@ -0,0 +1,10 @@
+﻿using DocCareWeb.Domain.Enums;
+
+namespace DocCareWeb.Application.Dtos.Appointment
+{
+    public class AppointmentStatusDto
+    {
+        public int Id { get; set; }
+        public AppointmentStatus Status { get; set; }
+    }
+}
