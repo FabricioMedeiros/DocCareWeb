12d5172eba0a0dda44d4d562e2b83cfa4f215efe
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-30 11:19:39 -0300

    Alteracao na entidade Appointment para permitir mudar o status internamente pela aplicacao

--- backend/DocCareWeb/src/DocCareWeb.Domain/Entities/Appointment.cs
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/Appointment.cs
@@ -13,11 +13,52 @@ namespace DocCareWeb.Domain.Entities
         public DateTime AppointmentDate { get; set; }
         public TimeSpan AppointmentTime { get; set; }
         public required decimal Cost { get; set; }
-        public AppointmentStatus Status { get; set; }
+        public AppointmentStatus Status { get; private set; }
         public string? Notes { get; set; }
         public DateTime CreatedAt { get; set; }
         public required string CreatedBy { get; set; }
         public DateTime? LastUpdatedAt { get; set; }
         public string? LastUpdatedBy { get; set; }
+
+        public void Schedule()
+        {
+            if (Status != AppointmentStatus.Scheduled)
+            {
+                throw new InvalidOperationException("A consulta deve estar no status 'Scheduled' para ser agendada.");
+            }
+
+            Status = AppointmentStatus.Scheduled;
+        }
+
+        public void Confirm()
+        {
+            if (Status != AppointmentStatus.Scheduled)
+            {
+                throw new InvalidOperationException("A consulta deve estar no status 'Scheduled' para ser confirmada.");
+            }
+
+            Status = AppointmentStatus.Confirmed;
+        }
+
+        public void Cancel()
+        {
+            if (Status == AppointmentStatus.Completed)
+            {
+                throw new InvalidOperationException("Não é possível cancelar uma consulta já completada.");
+            }
+
+            Status = AppointmentStatus.Canceled;
+        }
+
+        public void Complete()
+        {
+            if (Status != AppointmentStatus.Confirmed)
+            {
+                throw new InvalidOperationException("A consulta deve estar no status 'Confirmed' para ser completada.");
+            }
+
+            Status = AppointmentStatus.Completed;
+        }
     }
 }
+
