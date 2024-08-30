9a63ae7de07b70db30479feeff223df3163b15d5
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-08-30 10:00:23 -0300

    Alteracao para injetar o repositorio do paciente direamente no servico ao inves de injetar o genericservice passando o paciente, isso foi necessario pois o repositorio do paciente possui trataivas especificas para as consultas

--- backend/DocCareWeb/src/DocCareWeb.Application/Services/PatientService.cs
+++ backend/DocCareWeb/src/DocCareWeb.Application/Services/PatientService.cs
@@ -2,22 +2,20 @@
 using DocCareWeb.Application.Dtos.Patient;
 using DocCareWeb.Application.Interfaces;
 using DocCareWeb.Application.Notifications;
+using DocCareWeb.Application.Services;
 using DocCareWeb.Domain.Entities;
 using DocCareWeb.Domain.Interfaces;
 using FluentValidation;
 
-namespace DocCareWeb.Application.Services
+public class PatientService : GenericService<Patient, PatientCreateDto, PatientUpdateDto, PatientListDto>, IPatientService
 {
-    public class PatientService : GenericService<Patient, PatientCreateDto, PatientUpdateDto, PatientListDto>, IPatientService
+    public PatientService(
+        IPatientRepository patientRepository,
+        IValidator<PatientCreateDto> createValidator,
+        IValidator<PatientUpdateDto> updateValidator,
+        IMapper mapper,
+        INotificator notificator)
+        : base(patientRepository, createValidator, updateValidator, mapper, notificator)
     {
-        public PatientService(
-            IGenericRepository<Patient> patientRepository,
-            IValidator<PatientCreateDto> createValidator,
-            IValidator<PatientUpdateDto> updateValidator,
-            IMapper mapper,
-            INotificator notificator)
-            : base(patientRepository, createValidator, updateValidator, mapper, notificator)
-        {
-        }
     }
 }
