524e346546f7d34028ee57588ef1221159c6b010
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-09 21:19:00 -0300

    Criacao dos componentes basicos de navegacao da aplicacao

--- frontend/doccareweb/src/app/app.module.ts
+++ frontend/doccareweb/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/navigation/header/header.component';
+import { SidebarComponent } from './features/navigation/sidebar/sidebar.component';
+import { FooterComponent } from './features/navigation/footer/footer.component';
+import { LogoComponent } from './features/navigation/logo/logo.component';
+import { HomeComponent } from './features/navigation/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    LogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
