bbb0f8a9656e5eaf83a156b76dd87b1990486f64
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-16 20:15:18 -0300

    Adicionada a rota do componente de registro de usuarios nas rotas da aplicacao

--- frontend/doccareweb/src/app/app-routing.module.ts
+++ frontend/doccareweb/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/navigation/home/home.component';
 import { LoginComponent } from './features/account/login/login.component';
+import { RegisterComponent } from './features/account/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent }, 
+  { path: 'register', component: RegisterComponent }, 
   { path: '**', redirectTo: '' } 
 ];
 
