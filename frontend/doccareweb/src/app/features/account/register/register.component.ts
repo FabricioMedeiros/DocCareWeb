b93b5eefa16ef9942f9d09905254fac81600ba5b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-16 20:14:15 -0300

    Criacao e estilizacao do componente de registro de usuarios

---
+++ frontend/doccareweb/src/app/features/account/register/register.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+}
