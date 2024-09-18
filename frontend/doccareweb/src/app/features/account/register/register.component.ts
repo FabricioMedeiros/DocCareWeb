14d083a2c8fb3901d17014c2a11193cbb087ff17
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-18 10:04:44 -0300

    Implementacao das validacoes dos campos, e do controle de registro de usuario

--- frontend/doccareweb/src/app/features/account/register/register.component.ts
+++ frontend/doccareweb/src/app/features/account/register/register.component.ts
@@ -1,19 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, FormControlName, Validators } from '@angular/forms';
+import { Observable, fromEvent, merge } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { AuthService } from '../services/auth.service';
+import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/utils/generic-form-validation';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, AfterViewInit {
+
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements!: ElementRef[];
+
+  registerForm!: FormGroup;
+  user!: User;
+  errors: any[] = [];
+  
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {};
+  
+  changesSaved: boolean = true;
   showPassword: boolean = false;
-  showConfirmPassword: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.validationMessages = {
+      name: {
+        required: 'Informe o nome',
+      },
+      email: {
+        required: 'Informe o e-mail',
+        email: 'E-mail inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      }
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const controlBlurs: Observable<any>[] = this.formInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processMessages(this.registerForm);
+      this.changesSaved = false;
+    });
+  }
+
+  registerUser(): void {
+    if (this.registerForm.dirty && this.registerForm.valid) {
+      this.user = Object.assign({}, this.user, this.registerForm.value);
+
+      this.authService.registerUser(this.user).subscribe({
+        next: (success) => this.processSuccess(success),
+        error: (error) => this.processFail(error)
+      });
+
+      this.changesSaved = true;
+    }
+  }
+
+  processSuccess(response: any): void {
+    this.registerForm.reset();
+    this.errors = [];
+
+    this.authService.LocalStorage.saveLocalUserData(response);
+
+    const toast = this.toastr.success('Cadastro realizado com sucesso!', 'Bem-vindo!');
+
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+    }
+  }
+
+  processFail(fail: any): void {
+    this.errors = fail.error.errors;
+    console.log('erros retornados: ' + this.errors);
+    this.toastr.error('Ocorreu um erro.', 'Atenção');
+  }
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
-
-  toggleConfirmPasswordVisibility(): void {
-    this.showConfirmPassword = !this.showConfirmPassword;
-  }
+  
 }
