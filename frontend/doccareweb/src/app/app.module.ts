6a4449480f527fb64bcd640975605072ee1bfd65
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-18 10:10:36 -0300

    Criacao e registro do componente menu-login que ficara responsavel por exibir o nome do usuario e a opcao de logout no header da aplicacao.

--- frontend/doccareweb/src/app/app.module.ts
+++ frontend/doccareweb/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HeaderComponent } from './features/navigation/header/header.component';
 import { SidebarComponent } from './features/navigation/sidebar/sidebar.component';
 import { FooterComponent } from './features/navigation/footer/footer.component';
@@ -10,10 +15,7 @@ import { LogoComponent } from './features/navigation/logo/logo.component';
 import { HomeComponent } from './features/navigation/home/home.component';
 import { LoginComponent } from './features/account/login/login.component';
 import { RegisterComponent } from './features/account/register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuLoginComponent } from './features/navigation/menu-login/menu-login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LogoComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MenuLoginComponent
   ],
   imports: [
     BrowserModule,
