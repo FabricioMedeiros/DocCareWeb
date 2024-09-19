543d2676dd707eecf83f8be9f41ec0e3f59fc31f
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-19 08:57:44 -0300

    Criacao do guarda de rota do modulo Account para que apos o usuario esteja logado ele nao consiga acessar as rotas de login e register.

---
+++ frontend/doccareweb/src/app/features/account/services/account.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivateFn, CanDeactivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { RegisterComponent } from '../register/register.component';
+import { inject } from '@angular/core';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
+
+export const canActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const localStorageUtils = inject(LocalStorageUtils);
+  const router = inject(Router);
+  const token = localStorageUtils.getTokenUser();
+
+  if (token) {
+    console.log('Usuário logado, redirecionando para home.');
+    router.navigate(['/home']);
+    return false;
+  }
+  console.log('Usuário não logado, permitindo acesso.');
+  return true;
+};
+
+export const canDeactivate: CanDeactivateFn<RegisterComponent> = (component: RegisterComponent, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  if (!component.changesSaved) {
+    return window.confirm('Tem certeza que deseja abandonar o preenchimento do cadastro?');
+  }
+  return true;
+};
