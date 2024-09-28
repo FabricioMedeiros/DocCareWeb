8f285e05be3ba3cf541945d6a7abbf2355bf8bbf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-28 10:11:08 -0300

    Criacao do modulo compartilhado

---
+++ frontend/doccareweb/src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+   ]
+})
+export class SharedModule { }
