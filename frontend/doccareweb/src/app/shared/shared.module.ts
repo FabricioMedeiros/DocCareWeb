d78661748725b9213391b62349f1d59e0bb844aa
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-30 20:52:06 -0300

    Registrando os componentes  SearchBarComponent e PaginationComponent no modulo compartilhado.

--- frontend/doccareweb/src/app/shared/shared.module.ts
+++ frontend/doccareweb/src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
+    PaginationComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule,
-    BrowserModule,
     FormsModule,
     ReactiveFormsModule
   ],
   exports: [
-   ]
+    PaginationComponent,
+    SearchBarComponent
+  ]
 })
 export class SharedModule { }
