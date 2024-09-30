e1375a9ea4b9319505e185fbb9d6ec3bd267e91f
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-09-30 20:42:30 -0300

    Criacao do componente search-bar para realizar a pesquisa nas telas de listagem da aplicacao.

---
+++ frontend/doccareweb/src/app/shared/components/search-bar/search-bar.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  @Input() placeholder: string = 'Pesquisar...'; 
+  @Input() pageSizeOptions: number[] = [10, 30, 50]; 
+  @Output() search: EventEmitter<{ pageSize: number, term: string }> = new EventEmitter();
+  @Output() clear: EventEmitter<void> = new EventEmitter<void>(); 
+
+  selectedPageSize: number = 10; 
+  searchTerm: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onSearch(): void {
+    this.emitSearch();
+  }
+
+  onPageSizeChange(): void {
+    this.emitSearch(); 
+  }
+
+  clearSearch(): void {
+    this.searchTerm = '';
+    this.emitSearch();
+    this.clear.emit(); 
+  }
+
+  private emitSearch(): void {
+    this.search.emit({
+      pageSize: this.selectedPageSize,
+      term: this.searchTerm.trim()
+    });
+  }
+}
