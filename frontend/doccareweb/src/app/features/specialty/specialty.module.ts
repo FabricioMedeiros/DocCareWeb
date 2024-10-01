7c0f31c05a2f9c14b6ba2c6564551f41c51249a0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-01 10:11:43 -0300

    Registro dos componentes SpecialtyAppComponent, SpecialtyListComponent, do módulo compartilhado e do servico de esoecialidades

--- frontend/doccareweb/src/app/features/specialty/specialty.module.ts
+++ frontend/doccareweb/src/app/features/specialty/specialty.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SpecialtyRoutingModule } from './specialty-routing.module';
+import { SpecialtyAppComponent } from './specialty.app.component';
+import { SpecialtyListComponent } from './components/specialty-list/specialty-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SpecialtyService } from './services/specialty.service';
+
+
 @NgModule({
   declarations: [
+    SpecialtyAppComponent,
+    SpecialtyListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SpecialtyRoutingModule,
+    SharedModule
   ],
-  providers:[]
+  providers:[SpecialtyService]
 })
 export class SpecialtyModule { }
