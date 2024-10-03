60e19c51ef098cbe33743fb78fc4b7c8bd0d4a1b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-03 16:51:50 -0300

    Criacao da classe ErrorInterceptor para interceptar erros Http e redirecionar para a pagina correta de acordo com o erro.

---
+++ frontend/doccareweb/src/app/services/error.handler.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Router } from '@angular/router';
+
+import { NavigationService } from './navigation.service';
+import { LocalStorageUtils } from '../utils/localstorage';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private router: Router, private navigationService: NavigationService) { }
+
+    localStorageUtil = new LocalStorageUtils();
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse) {
+
+                if (error.status === 0) {
+                    this.navigationService.allowNavigation();
+                    this.router.navigate(['/service-unavailable']);
+                } else if (error.status === 401) {
+                    this.localStorageUtil.clearLocalUserData();
+                    this.router.navigate(['/account/login'], { queryParams: { returnUrl: this.router.url } });
+                }
+
+                return throwError(() => error);
+            }
+
+            return throwError(() => error);
+        }));
+    }
+}
