7e921f5e29a3b37fa0c54f783b9193040639568d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-03 16:55:12 -0300

    Configuracao de rotas dos componentes ServiceUnavailableComponent e NotFoundComponent

--- frontend/doccareweb/src/app/app-routing.module.ts
+++ frontend/doccareweb/src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './features/navigation/components/home/home.component';
 import { authGuard } from './features/navigation/services/auth.guard';
+import { NotFoundComponent } from './features/navigation/components/not-found/not-found.component';
+import { ServiceUnavailableComponent } from './features/navigation/components/service-unavailable/service-unavailable.component';
+import { navigationGuard } from './services/access.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'account/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'account', loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule) },
   { path: 'specialty', loadChildren: () => import('./features/specialty/specialty.module').then(m => m.SpecialtyModule) },
-  { path: '**', redirectTo: 'account/login' }
+  { path: 'service-unavailable', component: ServiceUnavailableComponent, canActivate: [navigationGuard] },
+  { path: 'not-found', component: NotFoundComponent, canActivate: [navigationGuard] },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
