28bef44562d4a28e8fc98fe69970866fffdcc8d9
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-09 15:35:18 -0300

    Configuracao dos modulos necessarios no HealthplanModule

--- frontend/doccareweb/src/app/features/healthplan/healthplan.module.ts
+++ frontend/doccareweb/src/app/features/healthplan/healthplan.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HealthPlanRoutingModule } from './components/healthplan-routing.module';
 import { HealthplanComponent } from './healthplan.component';
 import { HealthplanListComponent } from './components/healthplan-list/healthplan-list.component';
 import { HealthplanFormComponent } from './components/healthplan-form/healthplan-form.component';
-
-
+import { HealthPlanService } from './services/healthplan.service';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { HealthplanFormComponent } from './components/healthplan-form/healthplan
     HealthplanFormComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    HealthPlanRoutingModule,
+    SharedModule,
+    NgxSpinnerModule,
+    CurrencyMaskModule
+  ],
+  providers: [HealthPlanService]
 })
 export class HealthplanModule { }
