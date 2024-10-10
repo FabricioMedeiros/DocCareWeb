4b2f953f485607bfe35eb279b6e32dabd79a1d84
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-10 09:40:43 -0300

    Criacao dos componentes DoctorListComponent e DoctorFormComponent

--- frontend/doccareweb/src/app/features/doctor/doctor.module.ts
+++ frontend/doccareweb/src/app/features/doctor/doctor.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DoctorRoutingModule } from './doctor-routing.module';
 import { DoctorComponent } from './doctor.component';
+import { DoctorFormComponent } from './components/doctor-form/doctor-form.component';
+import { DoctorListComponent } from './components/doctor-list/doctor-list.component';
 
 
 @NgModule({
   declarations: [
-    DoctorComponent
+    DoctorComponent,
+    DoctorFormComponent,
+    DoctorListComponent
   ],
   imports: [
     CommonModule,
