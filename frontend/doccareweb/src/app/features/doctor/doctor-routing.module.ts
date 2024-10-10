69d12c516ef95b1f5a815894563db66011bda8d8
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-10 12:09:45 -0300

    Configuracao das rotas do modulo doctor

--- frontend/doccareweb/src/app/features/doctor/doctor-routing.module.ts
+++ frontend/doccareweb/src/app/features/doctor/doctor-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from 'src/app/core/guards/auth.guard';
+import { canDeactivateForm } from 'src/app/core/guards/can-deactivate-form.guard';
+import { genericResolver } from 'src/app/core/resolvers/generic-resolver';
+import { DoctorFormComponent } from './components/doctor-form/doctor-form.component';
+import { DoctorListComponent } from './components/doctor-list/doctor-list.component';
+import { DoctorComponent } from './doctor.component';
+import { DoctorService } from './services/doctor.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DoctorComponent,
+    children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: DoctorListComponent,  canActivate: [authGuard]  }, 
+      { path: 'new', component: DoctorFormComponent,  canActivate: [authGuard], canDeactivate: [canDeactivateForm] }, 
+      { path: 'edit/:id', component: DoctorFormComponent, 
+        canActivate: [authGuard],  
+        canDeactivate: [canDeactivateForm], resolve: {
+        doctor: genericResolver(DoctorService, (service, id) => service.getSpecialtyById(id))
+      }},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
