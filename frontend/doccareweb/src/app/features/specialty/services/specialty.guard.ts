c6e230778f9fd9eec2b9b19ef7b6bec0d578568a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-15 11:28:04 -0300

    Exclusao do guarda de rota do modulo SpecialtyModule pois nao sera mais necessario devido ao genericResolver

--- frontend/doccareweb/src/app/features/specialty/services/specialty.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivateFn, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { SpecialtyFormComponent } from '../components/specialty-form/specialty-form.component';
-
-export const canDeactivateSpecialtyForm: CanDeactivateFn<SpecialtyFormComponent> = (component: SpecialtyFormComponent, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  if (!component.changesSaved) {
-    return window.confirm('Tem certeza que deseja abandonar o preenchimento do cadastro?');
-  }
-  return true;
-};
