1c8e2e29106586d7731ae297f0163643f3d6bbb7
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-15 10:33:09 -0300

    Registro dos pipes CurrencyFormatPipe, PhoneMaskPipe, CrmMaskPipe  no SharedModule

--- frontend/doccareweb/src/app/shared/shared.module.ts
+++ frontend/doccareweb/src/app/shared/shared.module.ts
@@ -2,27 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
-import { CurrencyFormatPipe } from './pipes/currencyformat.pipe';
-
+import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
+import { SpecialtyService } from './services/specialty.service';
+import { DoctorService } from './services/doctor.service';
+import { PhoneMaskPipe } from './pipes/phone-mask.pipe';
+import { CrmMaskPipe } from './pipes/crm-mask.pipe';
 
 @NgModule({
   declarations: [
     PaginationComponent,
     SearchBarComponent,
-    CurrencyFormatPipe 
+    CurrencyFormatPipe,
+    PhoneMaskPipe,
+    CrmMaskPipe 
   ],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskDirective, 
+    NgxMaskPipe 
   ],
   exports: [
     PaginationComponent,
     SearchBarComponent,
-    CurrencyFormatPipe 
+    CurrencyFormatPipe,
+    PhoneMaskPipe,
+    CrmMaskPipe,
+    NgxMaskDirective, 
+    NgxMaskPipe 
   ],
-  providers: []
+  providers: [provideNgxMask(), SpecialtyService, DoctorService]
 })
 export class SharedModule { }
