7abcb7d6084b520a51dac956863ed1798095bc20
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-18 14:27:25 -0300

    Registrando os modulos necessarios na utilizacao do PatientModule

--- frontend/doccareweb/src/app/features/patient/patient.module.ts
+++ frontend/doccareweb/src/app/features/patient/patient.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { PatientRoutingModule } from './patient-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { PatientComponent } from './patient.component';
 import { PatientFormComponent } from './components/patient-form/patient-form.component';
 import { PatientListComponent } from './components/patient-list/patient-list.component';
 
+
 @NgModule({
-  declarations: [
-  
+  declarations: [  
     PatientComponent,
-       PatientFormComponent,
-       PatientListComponent
+    PatientFormComponent,
+    PatientListComponent
   ],
   imports: [
     CommonModule,
-    PatientRoutingModule
+    PatientRoutingModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    SharedModule
   ]
 })
 export class PatientModule { }
