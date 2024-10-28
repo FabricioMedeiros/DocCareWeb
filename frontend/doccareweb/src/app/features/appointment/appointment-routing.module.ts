e7ccd81e3076b06c2e3b641e95d650be1f74b1e0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-28 19:57:18 -0300

    Configuracao das rotas do modulo AppointmentModule

--- frontend/doccareweb/src/app/features/appointment/appointment-routing.module.ts
+++ frontend/doccareweb/src/app/features/appointment/appointment-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { authGuard } from 'src/app/core/guards/auth.guard';
+import { canDeactivateForm } from 'src/app/core/guards/can-deactivate-form.guard';
+import { genericResolver } from 'src/app/core/resolvers/generic-resolver';
+
+import { AppointmentComponent } from './appointment.component';
+import { AppointmentFormComponent } from './components/appointment-form/appointment-form.component';
+import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
+import { AppointmentService } from '../../shared/services/appointment.service';
+
+const routes: Routes = [
+  {
+    path: '', component: AppointmentComponent,
+    children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: AppointmentListComponent,  canActivate: [authGuard]  }, 
+      { path: 'new', component: AppointmentFormComponent,  canActivate: [authGuard], canDeactivate: [canDeactivateForm] }, 
+      { path: 'edit/:id', component: AppointmentFormComponent, 
+        canActivate: [authGuard],  
+        canDeactivate: [canDeactivateForm], 
+        resolve: { appointment: genericResolver(AppointmentService, (service, id) => service.getAppointmentById(id))
+      }},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
