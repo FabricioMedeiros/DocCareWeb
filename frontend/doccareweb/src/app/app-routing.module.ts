5b010804cba8064a2fbae3da79b7c31dd0abb1dc
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-10-28 19:53:53 -0300

    Configuracao da rota do modulo AppointmentModule no MainModule

--- frontend/doccareweb/src/app/app-routing.module.ts
+++ frontend/doccareweb/src/app/app-routing.module.ts
@@ -11,10 +11,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'account/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'account', loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule) },
-  { path: 'specialty', loadChildren: () => import('./features/specialty/specialty.module').then(m => m.SpecialtyModule) },
-  { path: 'healthplan', loadChildren: () => import('./features/healthplan/healthplan.module').then(m => m.HealthplanModule) },
+  { path: 'appointment', loadChildren: () => import('./features/appointment/appointment.module').then(m => m.AppointmentModule) },  
   { path: 'doctor', loadChildren: () => import('./features/doctor/doctor.module').then(m => m.DoctorModule) },
+  { path: 'healthplan', loadChildren: () => import('./features/healthplan/healthplan.module').then(m => m.HealthplanModule) },
   { path: 'patient', loadChildren: () => import('./features/patient/patient.module').then(m => m.PatientModule) },
+  { path: 'specialty', loadChildren: () => import('./features/specialty/specialty.module').then(m => m.SpecialtyModule) },
   { path: 'service-unavailable', component: ServiceUnavailableComponent, canActivate: [navigationGuard] },
   { path: 'not-found', component: NotFoundComponent, canActivate: [navigationGuard] },
   { path: '**', component: NotFoundComponent }
