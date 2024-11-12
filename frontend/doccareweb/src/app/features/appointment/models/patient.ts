81298c6afebda3bceaa84b993bee683836b0fb55
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-11 21:13:44 -0300

    Adicionada a propriedade de plano de saude no paciente para exibir essa informacao no cadastro de agendamento de consultas

--- frontend/doccareweb/src/app/features/appointment/models/patient.ts
+++ frontend/doccareweb/src/app/features/appointment/models/patient.ts
@@ -1,6 +1,10 @@
+import { HealthPlan } from "./healthplan";
+
 export interface Patient {
+    id: number;
     name: string;
     cpf: string;
     phone: string;
     cellPhone: string;
+    healthPlan: HealthPlan;
 }
