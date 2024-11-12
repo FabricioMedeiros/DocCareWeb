3a2699f8475e67b5bedf332ed33dc3db59fcdd91
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-12 17:23:17 -0300

    Criacao das dtos "BasicInfo" para retornar informacoes basicas de medicos, pacientes e plano de saude na listagem de agendamentos

---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Doctor/DoctorBasicInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DocCareWeb.Application.Dtos.Doctor
+{
+    public class DoctorBasicInfoDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/Patient/PatientBasicInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DocCareWeb.Application.Dtos.Patient
+{
+    public class PatientBasicInfoDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Cpf { get; set; }
+        public string? Phone { get; set; }
+        public string? CellPhone { get; set; }
+    }
+}
