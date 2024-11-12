ddb30356326b81d9fa5291d45b4660f39f33dd03
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-12 09:34:36 -0300

    Criacao do metodo formatDateToYYYYMMDD para formatacao de data, e do metodo convertTimeToMilliseconds para conversao de hora

--- frontend/doccareweb/src/app/core/utils/date-utils.ts
+++ frontend/doccareweb/src/app/core/utils/date-utils.ts
@@ -1,10 +1,38 @@
 export class DateUtils {
-    public static formatDateToDDMMYYYY(dateString: string): string {
-        const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
+  public static formatDateToDDMMYYYY(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
 
-        return `${year}-${month}-${day}`;
+    return `${year}-${month}-${day}`;
+  }
+
+  public static formatDateToYYYYMMDD(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
+
+  public static convertTimeToMilliseconds(time: any): number {
+    if (time instanceof Date) {
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      return (hours * 60 + minutes) * 60000;
     }
+
+    if (typeof time === 'string' && time.trim()) {
+      const [hours, minutes] = time.split(':').map(num => parseInt(num, 10));
+      if (isNaN(hours) || isNaN(minutes)) {
+        console.error('Hora inválida:', time);
+        return 0;
+      }
+      return (hours * 60 + minutes) * 60000;
+    }
+
+    console.error('Hora inválida:', time);
+    return 0;
+  }
 }
