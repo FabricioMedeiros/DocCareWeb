6adde78b691345df8dfcd5cd0ac15f91eb4976ef
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-12 09:09:45 -0300

    Configuracao para o AppointmentComponent exibir as rotas do modulo AppointmentModule.

--- frontend/doccareweb/src/app/features/appointment/appointment.component.ts
+++ frontend/doccareweb/src/app/features/appointment/appointment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-appointment',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent {
+ 
 
 }
