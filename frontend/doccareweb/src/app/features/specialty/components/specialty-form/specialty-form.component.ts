9b8ece7f929beffcb5f115b0391771ac5941cf6b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-21 20:09:32 -0300

    Refatoracao dos componentes SpecialtyListComponent,SpecialtyFormComponent e do serviço SpecialtyService atividades realizadas:
    1-  Aplicação da herancas dos componentes BaseList e BaseForm
    2 - Alteracao do nome dos metodos de getAll e delete no servico
    3 - Reestilização do layout dos componentes

--- frontend/doccareweb/src/app/features/specialty/components/specialty-form/specialty-form.component.ts
+++ frontend/doccareweb/src/app/features/specialty/components/specialty-form/specialty-form.component.ts
@@ -1,92 +1,73 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
-import { FormBuilder, FormControlName, FormGroup, Validators } from '@angular/forms';
-import { Specialty } from '../../models/specialty';
-import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/core/validators/generic-form-validation';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+
 import { ToastrService } from 'ngx-toastr';
-import { SpecialtyService } from '../../../../shared/services/specialty.service';
-import { fromEvent, merge, Observable } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { FormCanDeactivate } from 'src/app/core/guards/form-can-deactivate.interface';
+
+import { Specialty } from '../../models/specialty';
+import { SpecialtyService } from '../../../../shared/services/specialty.service';
+import { BaseFormComponent } from 'src/app/shared/components/base-form/base-form.component';
+import { GenericValidator } from 'src/app/core/validators/generic-form-validation';
 
 @Component({
   selector: 'app-specialty-form',
   templateUrl: './specialty-form.component.html',
   styleUrls: ['./specialty-form.component.css']
 })
-export class SpecialtyFormComponent implements OnInit, AfterViewInit, FormCanDeactivate  {
-  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[] = [];
+export class SpecialtyFormComponent extends BaseFormComponent<Specialty> implements OnInit {
 
-  errors: any[] = [];
-  specialtyForm!: FormGroup;
-  specialty!: Specialty;
-
-  validationMessages!: ValidationMessages;
-  genericValidator!: GenericValidator;
-  displayMessage: DisplayMessage = {};
-
-  changesSaved: boolean = true;
-  isEditMode: boolean = false;
-
-  constructor(private fb: FormBuilder,
+  constructor(
+    protected override fb: FormBuilder,
     private specialtyService: SpecialtyService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService) {
+    protected override router: Router,
+    protected override route: ActivatedRoute,
+    protected override toastr: ToastrService,
+    protected override spinner: NgxSpinnerService
+  ) {
+    super(fb, router, route, toastr, spinner);
     this.validationMessages = {
       description: {
         required: 'Informe a Descrição',
       }
     };
-
     this.genericValidator = new GenericValidator(this.validationMessages);
   }
 
   ngOnInit(): void {
-    this.specialtyForm = this.fb.group({
-      id: ['', []],
-      description: ['', [Validators.required]],
-    });
+    this.buildForm();
 
     const resolvedData = this.route.snapshot.data['specialty']; 
 
     if (resolvedData) {
-      this.spinner.show();
-      this.isEditMode = true;
-      this.specialtyForm.patchValue(resolvedData.data);
-      this.spinner.hide();
-    }         
+      this.initializeForm(resolvedData);  
+    }
   }
 
-  ngAfterViewInit(): void {
-    let controlBlurs: Observable<any>[] = this.formInputElements
-      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
-
-    merge(...controlBlurs).subscribe(() => {
-      this.displayMessage = this.genericValidator.processMessages(this.specialtyForm);
-
-      this.changesSaved = false;
+  buildForm(): void {
+    this.form = this.fb.group({
+      id: ['', []],
+      description: ['', [Validators.required]],
     });
   }
 
   saveSpecialty() {
-    if (this.specialtyForm.dirty && this.specialtyForm.valid) {
-      this.specialty = Object.assign({}, this.specialty, this.specialtyForm.value);
+    if (this.form.dirty && this.form.valid) {
+      this.entity = Object.assign({}, this.entity, this.form.value);
 
       if (this.isEditMode) {
-        this.specialtyService.updateSpecialty(this.specialty).subscribe({
-          next: (success) => {
-            this.processSuccess(success);
+        this.specialtyService.updateSpecialty(this.entity).subscribe({
+          next: () => {
+            this.processSuccess('Especialidade alterada com sucesso!', '/specialty/list');
           },
           error: (error) => {
             this.processFail(error);
           }
         });
       } else {
-        this.specialtyService.registerSpecialty(this.specialty).subscribe({
-          next: (success) => {
-            this.processSuccess(success);
+        this.specialtyService.registerSpecialty(this.entity).subscribe({
+          next: () => {
+            this.processSuccess('Especialidade cadastrada com sucesso!', '/specialty/list');
           },
           error: (error) => {
             this.processFail(error);
@@ -97,27 +78,129 @@ export class SpecialtyFormComponent implements OnInit, AfterViewInit, FormCanDea
 
     this.changesSaved = true;
   }
-
-  processSuccess(success: Specialty) {
-    this.specialtyForm.reset();
-    this.errors = [];
-
-    let toast = this.toastr.success(this.isEditMode ? 'Especialidade alterada com sucesso!' : 'Especialidade cadastrada com sucesso!', 'Atenção!');
-
-    if (toast) {
-      toast.onHidden.subscribe(() => {
-        this.router.navigate(['/specialty/list']);
-      });
-    }
-  }
-
-  processFail(fail: any) {
-    this.errors = fail.error.errors;
-    this.toastr.error('Ocorreu um erro.', 'Atenção');
-  }
-
-  cancel() {
-    this.router.navigate(['/specialty/list']); 
-  }  
 }
+
+
+// import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+// import { FormBuilder, FormControlName, FormGroup, Validators } from '@angular/forms';
+// import { Specialty } from '../../models/specialty';
+// import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/core/validators/generic-form-validation';
+// import { Router, ActivatedRoute } from '@angular/router';
+// import { ToastrService } from 'ngx-toastr';
+// import { SpecialtyService } from '../../../../shared/services/specialty.service';
+// import { fromEvent, merge, Observable } from 'rxjs';
+// import { NgxSpinnerService } from 'ngx-spinner';
+// import { FormCanDeactivate } from 'src/app/core/guards/form-can-deactivate.interface';
+
+// @Component({
+//   selector: 'app-specialty-form',
+//   templateUrl: './specialty-form.component.html',
+//   styleUrls: ['./specialty-form.component.css']
+// })
+// export class SpecialtyFormComponent implements OnInit, AfterViewInit, FormCanDeactivate  {
+//   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[] = [];
+
+//   errors: any[] = [];
+//   specialtyForm!: FormGroup;
+//   specialty!: Specialty;
+
+//   validationMessages!: ValidationMessages;
+//   genericValidator!: GenericValidator;
+//   displayMessage: DisplayMessage = {};
+
+//   changesSaved: boolean = true;
+//   isEditMode: boolean = false;
+
+//   constructor(private fb: FormBuilder,
+//     private specialtyService: SpecialtyService,
+//     private router: Router,
+//     private route: ActivatedRoute,
+//     private toastr: ToastrService,
+//     private spinner: NgxSpinnerService) {
+//     this.validationMessages = {
+//       description: {
+//         required: 'Informe a Descrição',
+//       }
+//     };
+
+//     this.genericValidator = new GenericValidator(this.validationMessages);
+//   }
+
+//   ngOnInit(): void {
+//     this.specialtyForm = this.fb.group({
+//       id: ['', []],
+//       description: ['', [Validators.required]],
+//     });
+
+//     const resolvedData = this.route.snapshot.data['specialty']; 
+
+//     if (resolvedData) {
+//       this.spinner.show();
+//       this.isEditMode = true;
+//       this.specialtyForm.patchValue(resolvedData.data);
+//       this.spinner.hide();
+//     }         
+//   }
+
+//   ngAfterViewInit(): void {
+//     let controlBlurs: Observable<any>[] = this.formInputElements
+//       .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+//     merge(...controlBlurs).subscribe(() => {
+//       this.displayMessage = this.genericValidator.processMessages(this.specialtyForm);
+
+//       this.changesSaved = false;
+//     });
+//   }
+
+//   saveSpecialty() {
+//     if (this.specialtyForm.dirty && this.specialtyForm.valid) {
+//       this.specialty = Object.assign({}, this.specialty, this.specialtyForm.value);
+
+//       if (this.isEditMode) {
+//         this.specialtyService.updateSpecialty(this.specialty).subscribe({
+//           next: (success) => {
+//             this.processSuccess(success);
+//           },
+//           error: (error) => {
+//             this.processFail(error);
+//           }
+//         });
+//       } else {
+//         this.specialtyService.registerSpecialty(this.specialty).subscribe({
+//           next: (success) => {
+//             this.processSuccess(success);
+//           },
+//           error: (error) => {
+//             this.processFail(error);
+//           }
+//         });
+//       }
+//     }
+
+//     this.changesSaved = true;
+//   }
+
+//   processSuccess(success: Specialty) {
+//     this.specialtyForm.reset();
+//     this.errors = [];
+
+//     let toast = this.toastr.success(this.isEditMode ? 'Especialidade alterada com sucesso!' : 'Especialidade cadastrada com sucesso!', 'Atenção!');
+
+//     if (toast) {
+//       toast.onHidden.subscribe(() => {
+//         this.router.navigate(['/specialty/list']);
+//       });
+//     }
+//   }
+
+//   processFail(fail: any) {
+//     this.errors = fail.error.errors;
+//     this.toastr.error('Ocorreu um erro.', 'Atenção');
+//   }
+
+//   cancel() {
+//     this.router.navigate(['/specialty/list']); 
+//   }  
+// }
 
--- frontend/doccareweb/src/app/features/specialty/components/specialty-list/specialty-list.component.ts
+++ frontend/doccareweb/src/app/features/specialty/components/specialty-list/specialty-list.component.ts
@@ -1,166 +1,35 @@
-import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { Router, NavigationEnd, Event } from '@angular/router';
-
-import { filter, map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 import { Specialty } from '../../models/specialty';
 import { SpecialtyService } from '../../../../shared/services/specialty.service';
+import { BaseListComponent } from 'src/app/shared/components/base-list/base-list.component';
 
 @Component({
   selector: 'app-specialty-list',
   templateUrl: './specialty-list.component.html',
   styleUrls: ['./specialty-list.component.css']
 })
-export class SpecialtyListComponent implements OnInit {
-  public specialties: Specialty[] = [];
-  errorMessage: string = '';
-  selectedSpecialty!: Specialty;
-
-  currentPage: number = 1;
-  pageSize: number = 10;
-  totalPages: number = 1;
-  searchTerm: string = '';
-  loadingData: boolean = true;
-
-  @Input() placeholderSearch: string = 'Pesquise pela descrição';
-  @Input() initialTermSearch: string = '';
-
-  bsModalRef!: BsModalRef;
-  @ViewChild('deleteModal') deleteModal!: TemplateRef<any>;  
-
-  constructor(
-    private specialtyService: SpecialtyService, 
-    private router: Router,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService,
-    private modalService: BsModalService
-  ) {
-    this.router.events.pipe(
-      filter((e: Event): e is NavigationEnd => e instanceof NavigationEnd),
-      map((e: NavigationEnd) => e)
-    ).subscribe((event: NavigationEnd) => {
-      if (!event.url.includes('/specialty')) {
-        this.specialtyService.clearLocalCurrentPageList();
-        this.specialtyService.clearLocalSearchTerm();
-      } 
-    });
+export class SpecialtyListComponent extends BaseListComponent<Specialty> implements OnInit {
+  constructor(specialtyService: SpecialtyService,
+    router: Router,
+    toastr: ToastrService,
+    spinner: NgxSpinnerService,
+    modalService: BsModalService) {
+    super(specialtyService, router, toastr, spinner, modalService);
   }
 
-  ngOnInit(): void {
-    const storedPage = this.specialtyService.getLocalCurrentPageList();
-    const storedSearchTerm = this.specialtyService.getLocalSearchTerm();
-  
-    if (storedPage) {
-      this.currentPage = parseInt(storedPage, 10);
-    }
-  
-    if (storedSearchTerm) {
-      this.searchTerm = storedSearchTerm;
-      this.initialTermSearch = storedSearchTerm;
-    }
-
-    this.loadSpecialties(this.searchTerm);
+  override ngOnInit(): void {
+    this.fieldSearch = 'description';
+    this.placeholderSearch = 'Pesquise pela descrição';
+    super.ngOnInit();
   }
 
-  loadSpecialties(searchTerm?: string): void {
-    this.spinner.show();
-    this.loadingData = true;
-    this.specialties = [];
-    this.searchTerm = searchTerm ?? '';
-
-    this.specialtyService.getAllSpecialties(this.currentPage, this.pageSize, 'Description', this.searchTerm).subscribe({
-      next: response => {
-        this.processLoadSpecialtiesSuccess(response);
-      },
-      error: error => {
-        this.processLoadSpecialtiesFail(error);
-      },
-      complete: () => {
-        this.processCompleted();
-      }
-    });
-  }
-
-  private processLoadSpecialtiesSuccess(response: any) {
-    this.specialties = response.data.items;
-    this.currentPage = response.data.page;
-    this.totalPages = Math.ceil(response.data.totalRecords / response.data.pageSize);
-  }
-
-  private processLoadSpecialtiesFail(error: any) {
-    this.errorMessage = error.error?.errors?.[0] || 'Ocorreu um erro desconhecido.';
-    this.toastr.error('Ocorreu um erro.', 'Atenção');
-    this.spinner.hide();
-  }  
-
-  private processCompleted() {
-    this.loadingData = false;
-    this.spinner.hide();
-  }
-
-  addSpecialty() {
-    this.specialtyService.clearLocalCurrentPageList();
-    this.specialtyService.clearLocalSearchTerm();
-    this.currentPage = 1;
-    this.searchTerm = '';
-    this.router.navigate(['/specialty/new']);
-  }
-
-  editSpecialty(specialty: Specialty) {
-    this.specialtyService.saveLocalCurrentPageList(this.currentPage);
-    this.specialtyService.saveLocalSearchTerm(this.searchTerm);
-    this.router.navigate(['/specialty/edit', specialty.id]);
-  }
-
-  openDeleteModal(template: TemplateRef<any>, specialty: Specialty) {
-    this.selectedSpecialty = specialty;
-    this.bsModalRef = this.modalService.show(template, { class: 'custom-modal-delete' });
-  } 
-
-  confirmDelete(specialty: Specialty): void {  
-    this.specialtyService.deleteSpecialty(specialty.id).subscribe({
-      next: success => {
-        this.processSuccessDelete(success, specialty);
-      },
-      error: error => {
-        this.processFailDelete(error);
-      }
-    });
-  }
-
-  processSuccessDelete(response: any, specialty: Specialty) {
-    this.bsModalRef.hide();
-    this.specialties = this.specialties.filter(item => item.id !== specialty.id);    
-    this.toastr.success('Registro excluído com sucesso!', 'Atenção!');
-  }
-
-  processFailDelete(fail: any) {
-    this.toastr.error('Ocorreu um erro.', 'Atenção');
-  }
-
-  onSearch(event: { pageSize: number, term: string }): void {
-    this.pageSize = event.pageSize;
-    this.searchTerm = event.term;
-    this.currentPage = 1;
-    this.specialtyService.saveLocalSearchTerm(this.searchTerm);
-
-    this.loadSpecialties(this.searchTerm);
-  }
-
-  clearSearch(): void {
-    this.searchTerm = '';
-    this.currentPage = 1;
-    this.loadSpecialties();
-  }
-
-  onPageChanged(page: number) {
-    this.currentPage = page;
-    this.specialtyService.saveLocalCurrentPageList(this.currentPage);
-
-    this.loadSpecialties(this.searchTerm);
+  isCurrentRoute(url: string): boolean {
+    return url.includes('/specialty');
   }
 }
