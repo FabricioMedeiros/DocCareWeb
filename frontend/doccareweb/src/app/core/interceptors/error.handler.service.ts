1abb215ac42cdaba894faf560de7900ca47280b0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-11-25 10:47:12 -0300

    Alteracao para exibir a mensagem de sessao expirada ao tentar utiizar a aplicacao com o token vencido

--- frontend/doccareweb/src/app/core/interceptors/error.handler.service.ts
+++ frontend/doccareweb/src/app/core/interceptors/error.handler.service.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from "rxjs";
@@ -9,26 +10,38 @@ import { LocalStorageUtils } from '../utils/localstorage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router: Router, private navigationService: NavigationService) { }
+    constructor(
+        private router: Router,
+        private navigationService: NavigationService,
+        private toastr: ToastrService
+    ) { }
 
     localStorageUtil = new LocalStorageUtils();
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(catchError(error => {
-            if (error instanceof HttpErrorResponse) {
-
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
                 if (error.status === 0) {
                     this.navigationService.allowNavigation();
                     this.router.navigate(['/service-unavailable']);
                 } else if (error.status === 401) {
                     this.localStorageUtil.clearLocalUserData();
+
+                    this.toastr.clear();
+                    this.toastr.warning('Sessão encerrada. Faça login novamente.', 'Atenção');
                     this.router.navigate(['/account/login'], { queryParams: { returnUrl: this.router.url } });
+
+                    return throwError(() => new HttpErrorResponse({
+                        error: { message: 'Sessão encerrada.' },
+                        status: 401,
+                        statusText: 'Unauthorized'
+                    }));
+                } else {
+                    this.navigationService.allowNavigation();
                 }
 
                 return throwError(() => error);
-            }
-
-            return throwError(() => error);
-        }));
+            })
+        );
     }
 }
