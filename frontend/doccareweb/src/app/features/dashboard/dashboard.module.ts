a1029c9ab2ed36294a14e7feb8d19a8b0aea3e9a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-12-02 10:49:11 -0300

    Configuracao dos modulos necessarios no modulo DashboardModule

--- frontend/doccareweb/src/app/features/dashboard/dashboard.module.ts
+++ frontend/doccareweb/src/app/features/dashboard/dashboard.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardDataComponent } from './components/dashboard-data/dashboard-data.component';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 import { DashboardComponent } from './dashboard.component';
+import { DashBoardService } from './services/dasboard.service';
+import { SharedModule } from "../../shared/shared.module";
+
+
 
 @NgModule({
   declarations: [  
@@ -12,7 +20,11 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    FormsModule,
+    DashboardRoutingModule,
+    NgxSpinnerModule,
+    SharedModule
+],
+  providers:[DashBoardService]
 })
 export class DashboardModule { }
