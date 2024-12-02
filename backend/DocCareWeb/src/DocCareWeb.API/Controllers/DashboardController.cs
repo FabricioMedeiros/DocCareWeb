dae40520e5c7228332ac890de1d90418d34d3b89
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-12-02 20:03:17 -0300

    Criacao da controller DashboardController

---
+++ backend/DocCareWeb/src/DocCareWeb.API/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using DocCareWeb.Application.Interfaces;
+using DocCareWeb.Application.Notifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocCareWeb.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class DashboardController : MainController
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService, INotificator notificator) : base(notificator)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardData(int? doctorId = null, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            var data = await _dashboardService.GetDashboardDataAsync(doctorId, startDate, endDate);
+            return CustomResponse(data);
+        }
+
+    }
+}
