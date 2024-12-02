bf4962b751e2a1615ae54d0e86a8a7e8f6bf0104
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-12-02 11:31:03 -0300

    Criacao dos metodos GetAppointmentsByStatusAsync para retornar a listagem dos status dos agendamentos do dia podendo filtrar pelo medico, e do metodo GetFinancialSummaryAsync retornar o financeiro pendente e recebido em de um determinado periodo

--- backend/DocCareWeb/src/DocCareWeb.Infrastructure/Repositories/AppointmentRepository.cs
+++ backend/DocCareWeb/src/DocCareWeb.Infrastructure/Repositories/AppointmentRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DocCareWeb.Domain.Entities;
+using DocCareWeb.Domain.Enums;
 using DocCareWeb.Domain.Interfaces;
 using DocCareWeb.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -32,5 +33,45 @@ namespace DocCareWeb.Infrastructure.Repositories
                                 .Include(a => a.HealthPlan)
                                 .FirstOrDefaultAsync(a => a.Id == id);
         }
+
+        public async Task<Dictionary<string, int>> GetAppointmentsByStatusAsync(DateTime date, int? doctorId = null)
+        {
+            var query = _context.Set<Appointment>()
+                .Where(a => a.AppointmentDate.Date == date.Date);
+
+            if (doctorId.HasValue)
+            {
+                query = query.Where(a => a.DoctorId == doctorId.Value);
+            }
+
+            return await query
+                .GroupBy(a => a.Status)
+                .ToDictionaryAsync(g => g.Key.ToString(), g => g.Count());
+        }
+
+        public async Task<FinancialSummary> GetFinancialSummaryAsync(DateTime startDate, DateTime endDate, int? doctorId = null)
+        {
+            var receivedQuery = _context.Set<Appointment>()
+                .Where(a => a.Status == AppointmentStatus.Completed && a.AppointmentDate.Date >= startDate.Date && a.AppointmentDate.Date <= endDate.Date);
+
+            var pendingQuery = _context.Set<Appointment>()
+                .Where(a => a.Status == AppointmentStatus.Confirmed && a.AppointmentDate.Date >= startDate.Date && a.AppointmentDate.Date <= endDate.Date);
+
+            if (doctorId.HasValue)
+            {
+                receivedQuery = receivedQuery.Where(a => a.DoctorId == doctorId.Value);
+                pendingQuery = pendingQuery.Where(a => a.DoctorId == doctorId.Value);
+            }
+
+            var totalReceived = await receivedQuery.SumAsync(a => a.Cost);
+            var totalPending = await pendingQuery.SumAsync(a => a.Cost);
+
+            return new FinancialSummary
+            {
+                TotalReceived = totalReceived,
+                TotalPending = totalPending
+            };
+        }
+
     }
 }
