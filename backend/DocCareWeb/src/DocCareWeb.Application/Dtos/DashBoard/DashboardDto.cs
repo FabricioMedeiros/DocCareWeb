57fed56c1ca4a98f6c3b45e283b749e29446e4b0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-12-02 11:19:52 -0300

    Criacao da model de resumo financeiro e da dto da dashboard

---
+++ backend/DocCareWeb/src/DocCareWeb.Application/Dtos/DashBoard/DashboardDto.cs
@@ -0,0 +1,13 @@
+﻿using DocCareWeb.Domain.Entities;
+
+namespace DocCareWeb.Application.Dtos.DashBoard
+{
+    public class DashboardDto
+    {
+        public int TotalPatients { get; set; }
+        public required Dictionary<string, int> AppointmentsByStatus { get; set; }
+        public required FinancialSummary DailyFinancialSummary { get; set; }
+        public required FinancialSummary MonthlyFinancialSummary { get; set; }
+    }
+
+}
---
+++ backend/DocCareWeb/src/DocCareWeb.Domain/Entities/FinancialSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace DocCareWeb.Domain.Entities
+{
+    public class FinancialSummary
+    {
+        public decimal TotalPending { get; set; }
+        public decimal TotalReceived { get; set; }
+    }
+}
